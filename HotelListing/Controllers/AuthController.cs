bbf487f049ba51453307aa50127e01bebf6fd2c3
ehsan <ehsan.rp98@gmail.com>
2026-07-13 06:43:34 +0330

    implemented auth controller

---
+++ HotelListing/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+using HotelListing.DTOs.Auth;
+using HotelListing.Interfaces;
+using HotelListing.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListing.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IUsersService usersService) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<RegisteredUserDto>> Register(RegisterUserDto registerUserDto)
+        {
+            var result = await usersService.RegisterUserAsync(registerUserDto);
+
+            return ToActionResult(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(LoginUserDto loginUserDto)
+        {
+            var result = await usersService.LoginAsync(loginUserDto);
+            return ToActionResult(result);
+        }
+    }
+}
