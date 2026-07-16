704c5c0d6c87edb431f97ec5b21c3d308d448c3d
ehsan <ehsan.rp98@gmail.com>
2026-07-16 10:48:35 +0330

    Integrated API Key with database

--- HotelListing/Data/Configurations/ApiKeyConfigurations.cs
+++ HotelListing/Data/Configurations/ApiKeyConfigurations.cs
@@ -7,6 +7,15 @@ public class ApiKeyConfigurations : IEntityTypeConfiguration<ApiKey>
 {
     public void Configure(EntityTypeBuilder<ApiKey> builder)
     {
-        builder.HasIndex(k => k.Key).IsUnique();
+        builder.HasIndex(a => a.AppName).IsUnique();
+        builder.HasData(
+            new ApiKey
+            {
+                Id = 1,
+                AppName = "AppName",
+                CreatedAtUtc = new DateTime(2026, 01, 01),
+                Key = "OWZkOGE1MWUtMDgzMy00ZWYzLTlhNmYtY2I0ZmZhZGQ5OTYz"
+            }
+        );
     }
 }
---
+++ HotelListing/Migrations/20260716071247_AddedDefaultApiKey.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDefaultApiKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ApiKeys",
+                columns: new[] { "Id", "AppName", "CreatedAtUtc", "ExpiresAtUts", "Key" },
+                values: new object[] { 1, "AppName", new DateTimeOffset(new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 3, 30, 0, 0)), null, "OWZkOGE1MWUtMDgzMy00ZWYzLTlhNmYtY2I0ZmZhZGQ5OTYz" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApiKeys_AppName",
+                table: "ApiKeys",
+                column: "AppName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ApiKeys_AppName",
+                table: "ApiKeys");
+
+            migrationBuilder.DeleteData(
+                table: "ApiKeys",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
