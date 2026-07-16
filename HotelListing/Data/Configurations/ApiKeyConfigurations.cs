b7e3bff22e09ca2f82a1986efe963f5a2c0c22fe
ehsan <ehsan.rp98@gmail.com>
2026-07-16 09:09:32 +0330

    Exported Configurations to external file

---
+++ HotelListing/Data/Configurations/ApiKeyConfigurations.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.Data.Configurations;
+
+public class ApiKeyConfigurations : IEntityTypeConfiguration<ApiKey>
+{
+    public void Configure(EntityTypeBuilder<ApiKey> builder)
+    {
+        builder.HasIndex(k => k.Key).IsUnique();
+    }
+}
