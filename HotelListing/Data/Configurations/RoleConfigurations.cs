5763f7317ee1d6f62411ff525884c21f22246e84
ehsan <ehsan.rp98@gmail.com>
2026-08-02 17:26:23 +0330

    refactored NormalizedName to use static role names defined in UserRoleNames.cs

--- HotelListing/Data/Configurations/RoleConfigurations.cs
+++ HotelListing/Data/Configurations/RoleConfigurations.cs
@@ -10,9 +10,24 @@ public class RoleConfigurations : IEntityTypeConfiguration<IdentityRole>
     public void Configure(EntityTypeBuilder<IdentityRole> builder)
     {
         builder.HasData(
-            new IdentityRole {Id="bf768284-b943-4612-8e42-83097e51f6de",ConcurrencyStamp = "937de25d-ca6a-4f52-acb8-594f57e5a8c3", Name = UserRoleNames.Administrator, NormalizedName = "ADMINISTRATOR" },
-            new IdentityRole {Id="09b68774-a7c2-485b-bdda-70228b89ab91",ConcurrencyStamp = "829af75a-b871-47e8-b87a-4c2cf939c649",Name = UserRoleNames.User, NormalizedName = "USER" },
-            new IdentityRole {Id="6561611b-3696-4749-bbe5-5f347224b665",ConcurrencyStamp = "d523c0f3-7a3e-469f-a94a-f623a579fc85",Name = UserRoleNames.HotelAdmin, NormalizedName = "HOTEL ADMIN" }
+            new IdentityRole
+            {
+                Id = "bf768284-b943-4612-8e42-83097e51f6de", ConcurrencyStamp = "937de25d-ca6a-4f52-acb8-594f57e5a8c3",
+                Name = UserRoleNames.Administrator,
+                NormalizedName = UserRoleNames.Administrator.ToUpper()
+            },
+            new IdentityRole
+            {
+                Id = "09b68774-a7c2-485b-bdda-70228b89ab91", ConcurrencyStamp = "829af75a-b871-47e8-b87a-4c2cf939c649",
+                Name = UserRoleNames.User,
+                NormalizedName = UserRoleNames.User.ToUpper()
+            },
+            new IdentityRole
+            {
+                Id = "6561611b-3696-4749-bbe5-5f347224b665", ConcurrencyStamp = "d523c0f3-7a3e-469f-a94a-f623a579fc85",
+                Name = UserRoleNames.HotelAdmin,
+                NormalizedName = UserRoleNames.HotelAdmin.ToUpper()
+            }
         );
     }
 }
