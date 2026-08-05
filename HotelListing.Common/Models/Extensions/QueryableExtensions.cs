ef38713430acd84bcdc19169e0433059e6cf3a86
ehsan <ehsan.rp98@gmail.com>
2026-08-05 07:17:17 +0330

    Implemented pagination service

---
+++ HotelListing.Common/Models/Extensions/QueryableExtensions.cs
@@ -0,0 +1,38 @@
+﻿using HotelListing.Common.Models.Paging;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Common.Models.Extensions;
+
+public static class QueryableExtensions
+{
+    public static async Task<PagedResult<T>> ToPagedResultAsync<T>(
+        this IQueryable<T> source,
+        PaginationParameters paginationParameters)
+    {
+        var skipCount = (paginationParameters.PageNumber - 1) * paginationParameters.PageSize;
+
+        var totalCount = await source.CountAsync();
+        var data = await source
+            .Skip(skipCount)
+            .Take(paginationParameters.PageSize)
+            .ToListAsync();
+
+        var totalPages = (int)Math.Ceiling(totalCount / (double)paginationParameters.PageSize);
+
+        var metadata = new PaginationMetadata()
+        {
+            CurrentPage = paginationParameters.PageNumber,
+            PageSize = paginationParameters.PageSize,
+            TotalCount = totalCount,
+            TotalPages = totalPages,
+            HasNextPage = paginationParameters.PageNumber < totalPages,
+            HasPreviousPage = paginationParameters.PageNumber > 1
+        };
+
+        return new PagedResult<T>
+        {
+            Data = data,
+            Metadata = metadata
+        };
+    }
+}
---
+++ HotelListing.Common/Models/Paging/PaginationMetadata.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelListing.Common.Models.Paging;
+
+public class PaginationMetadata
+{
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages { get; set; }
+    public bool HasNextPage { get; set; }
+    public bool HasPreviousPage { get; set; }
+}
