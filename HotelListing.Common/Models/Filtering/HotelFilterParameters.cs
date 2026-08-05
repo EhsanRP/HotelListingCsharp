d38ad8dfd95c9b371245c435a339a9d61c54fcef
ehsan <ehsan.rp98@gmail.com>
2026-08-05 09:01:10 +0330

    Created HotelSortingEnum.cs after Hotel Properties to do a precice sorting

--- HotelListing.Common/Models/Filtering/HotelFilterParameters.cs
+++ HotelListing.Common/Models/Filtering/HotelFilterParameters.cs
@@ -3,9 +3,10 @@
 public class HotelFilterParameters : BaseFilterParameters
 {
     public int? CountryId { get; set; }
+    public HotelSortingEnum? HotelSorting { get; set; }
     public double? MinRating { get; set; }
     public double? MaxRating { get; set; }
-    public decimal MinPrice { get; set; }
-    public decimal MaxPrice { get; set; }
+    public decimal? MinPrice { get; set; }
+    public decimal? MaxPrice { get; set; }
     public string? Location { get; set; }
 }
---
+++ HotelListing.Common/Models/Filtering/HotelSortingEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelListing.Common.Models.Filtering;
+
+public enum HotelSortingEnum
+{
+    Name = 0,
+    Price = 1,
+    Rating = 2,
+}
