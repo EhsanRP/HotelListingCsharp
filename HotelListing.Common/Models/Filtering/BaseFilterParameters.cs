cd5cf088e754fb2e6d3315a381f84a5ba4382ed3
ehsan <ehsan.rp98@gmail.com>
2026-08-05 08:59:24 +0330

    Removed sortBy property due to different property fields to get String free property checking for sorting anchor

--- HotelListing.Common/Models/Filtering/BaseFilterParameters.cs
+++ HotelListing.Common/Models/Filtering/BaseFilterParameters.cs
@@ -3,6 +3,5 @@
 public abstract class BaseFilterParameters
 {
     public string? SearchString { get; set; }
-    public string? SortBy { get; set; }
     public bool SortDescending { get; set; } = false;
 }
